d506931d0c27fdb7d606b9f3e73a250b663a9725
prbehere93 <prbehere93@gmail.com>
2021-10-30 10:52:51 +0530

    hidden secret key

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import django_heroku
-# import os
+import os
 # from environs import Env
 
  
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "JNPwcxonGDd14f8vFgHyuzINez8SCiabhyZIzZcJGXk="
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "True"
